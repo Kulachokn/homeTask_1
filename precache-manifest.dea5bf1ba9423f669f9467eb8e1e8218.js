783e2927832ce09942ad8e7cc4d7806ada9aac98
Anastasiia Kulachok <kulachokn@gmail.com>
2020-02-20 22:50:15 +0200

    Updates

---
+++ precache-manifest.dea5bf1ba9423f669f9467eb8e1e8218.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae10f83f00f7e2e3df87c9cdb8538e71",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "a12541dd2fa172111857",
+    "url": "/react/static/css/main.4e995113.chunk.css"
+  },
+  {
+    "revision": "e876957ee45b038adfec",
+    "url": "/react/static/js/2.65085c4c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react/static/js/2.65085c4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a12541dd2fa172111857",
+    "url": "/react/static/js/main.71e3cd5c.chunk.js"
+  },
+  {
+    "revision": "f7fde5b7598b0095d53a",
+    "url": "/react/static/js/runtime-main.918d335b.js"
+  }
+]);
